3a854a8b63408b1963c62ca6feb6cee8c2ebc30f
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-25 14:35:19 +0330

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+//? redux
 import { useDispatch } from "react-redux";
 import { showMeState } from "./app/features/charectersSlice";
-
+//? apollo-gql
 import { useQuery } from "@apollo/client";
 import { GET_CHARACTERS } from "./gql/queries/getCharacters";
 
 const App = () => {
+  //! redux test
   const dispatch = useDispatch();
   dispatch(showMeState());
+  //! apollo test
   const { loading, error, data } = useQuery(GET_CHARACTERS(1));
   console.log({ loading, data, error });
   return <div>App</div>;
--- src/gql/queries/getInfo.js
+++ src/gql/queries/getInfo.js
@@ -1,5 +1,4 @@
 import { gql } from "@apollo/client";
-
 export const GET_INFO = gql`
   query getInfo {
     characters {
