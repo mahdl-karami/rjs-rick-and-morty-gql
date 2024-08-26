95058064950ede98a54fbef0674f4489a2f38263
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-26 15:29:47 +0330

    add bookmarks counter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,19 @@ import { FaRegBookmark } from "react-icons/fa6";
 import styles from "../styles/layout.module.css";
 //? router
 import { Link } from "react-router-dom";
+//? hooks
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { ids } = useSelector((state) => state.bookmarks);
   return (
     <header className={styles.header}>
       <span className={styles.bookmark}>
-        <Link to='/bookmarks'>
+        <Link to="/bookmarks">
           <FaRegBookmark />
         </Link>
       </span>
+      {ids?.length ? <span className={styles.count}>{ids.length}</span> : null}
       <h1>The Rick and Morty API</h1>
     </header>
   );
