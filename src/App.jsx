6bd00bba84216b7a28126e27fe339c0892028d4e
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-09-06 13:49:17 +0330

    clean app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 //? redux
 import { useDispatch, useSelector } from "react-redux";
-import { add, getLocalIds, showMeState } from "./app/features/bookmarkSlice";
+import { getLocalIds } from "./app/features/bookmarkSlice";
 //? apollo-gql
 import { useQuery } from "@apollo/client";
 import { GET_CHARACTERS } from "./gql/queries/getCharacters";
 import CharacterCard from "./components/CharacterCard";
 //? style modules
 import cardS from "./styles/card.module.css";
+//? components
 import Pagination from "./components/Pagination";
+//? hooks
 import { useEffect, useState } from "react";
 
 const App = () => {
@@ -24,7 +26,6 @@ const App = () => {
   }, []);
   //! apollo client
   const { loading, error, data } = useQuery(GET_CHARACTERS(page));
-  console.log({ loading, data, error });
   return (
     <div>
       <h2 className="title">All Characters</h2>
