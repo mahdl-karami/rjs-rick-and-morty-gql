325c8746a087be02ea50fdb364df715bb4eba8d9
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-09-06 13:47:49 +0330

    fix bookmarks/stoage at location page

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,15 +1,28 @@
 //? hooks
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
+import { useEffect } from "react";
 //? apollo-gql
 import { getLocation } from "../gql/queries/getLocation";
 //? components
 import CharacterCard from "../components/CharacterCard";
 //? styles
 import cardS from "../styles/card.module.css";
+//? redux
+import { getLocalIds } from "../app/features/bookmarkSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Locations = () => {
   const id = useParams()?.locId;
+  //! redux
+  const { ids } = useSelector((state) => state.bookmarks);
+  const dispatch = useDispatch();
+  //! localstorage
+  useEffect(() => {
+    if (!ids?.length) {
+      dispatch(getLocalIds(JSON.parse(localStorage.getItem("ids"))));
+    }
+  }, []);
   //! apollo client
   const { loading, error, data } = useQuery(getLocation(id));
   return (
